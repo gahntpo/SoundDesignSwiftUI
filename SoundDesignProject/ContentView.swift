fbef18e0b77e2f4e226bc622a8e839503776b00a
Karin Prater <karin.prater@gmx.de>
2020-09-05 14:45:31 +0300

    Initial Commit

---
+++ SoundDesignProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundDesignProject
+//
+//  Created by Karin Prater on 05.09.20.
+//  Copyright © 2020 Karin Prater. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
